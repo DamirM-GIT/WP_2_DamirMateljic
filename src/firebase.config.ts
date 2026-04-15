1e9df427fa15e3fa77c67e1624a6ff55892e0427
Administrator <mateljic.damir@gmail.com>
2026-04-15 09:18:47 -1100

    mala promjena css fajlova

---
+++ src/firebase.config.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCY7_IW10L9-sN5pZBvkF9bZbg5qAx9Hpg',
+  authDomain: 'wp2-damirmateljic.firebaseapp.com',
+  projectId: 'wp2-damirmateljic',
+  storageBucket: 'wp2-damirmateljic.firebasestorage.app',
+  messagingSenderId: '283523929999',
+  appId: '1:283523929999:web:6773727ad497aaffe95dfa',
+};
+export default firebaseConfig;
